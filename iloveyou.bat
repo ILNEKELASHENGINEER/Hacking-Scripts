43a8a14eb42a839b580ac9eeb5ddd4e0bd26faf7
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2022-10-25 23:02:17 +0530

    Create iloveyou.bat
    This is i love you virus!!! this is prank script you can prank you friends for fun
    don't play with this script on educational systems or any other innocent ones system
    save the file in .bat
    It wont hack or crash the system..it just shutdowns it..
    hola happy coding

---
+++ iloveyou.bat
@@ -0,0 +1,15 @@
+echo Hi do you love me (yes or no)
+set /p love=
+if %love%==yes goto love
+if %love%==no goto hate
+:love
+echo i love you too...
+echo Meet you soon :)
+pause
+exit
+:hate
+echo But I LOVE YOU...hehe eh eh
+echo you are hacked...
+echo Your Pc will crash in 5 seconds
+timeout 5
+shutdown -s -t 50
